2018d35f572dc3cd8bc2e8702eb4854bca1236e1
Jan Szumotalski <jszumotalski@gmail.com>
2025-04-20 19:42:20 +0200

    fix: Added a simple check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-cat >> /usr/local/apache2/conf/httpd.conf <<EOF
+if [ $DOMAIN -a $EMAIL]; then
+    if [ ! -e /usr/local/apache2/.configured ]; then
+        cat >> /usr/local/apache2/conf/httpd.conf <<EOF
 ServerName $DOMAIN
 ServerAdmin $EMAIL
 <VirtualHost *:80>
     ServerName $DOMAIN
 </VirtualHost>
 EOF
-httpd -k start
-certbot --apache -n --keep -d $DOMAIN -m $EMAIL
-httpd -k stop
+        httpd -k start
+        certbot --apache -n --keep -d $DOMAIN -m $EMAIL
+        httpd -k stop
+        touch /usr/local/apache2/.configured
+    fi
+fi
 httpd-foreground
