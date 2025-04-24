c767d55e0efba27be8d225bc3607e1f4be50ca47
Jan Szumotalski <jszumotalski@gmail.com>
2025-04-24 15:25:56 +0200

    fix: typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $DOMAIN -a $EMAIL]; then
+if [ $DOMAIN -a $EMAIL ]; then
     if [ ! -e /usr/local/apache2/.configured ]; then
         cat >> /usr/local/apache2/conf/httpd.conf <<EOF
 ServerName $DOMAIN
