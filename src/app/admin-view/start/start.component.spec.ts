0c60f39152a55eca87d65aaecec591392a01a76e
Jan Szumotalski <jszumotalski@gmail.com>
2025-06-01 21:48:56 +0200

    feat: Added menu items and account security to settings

--- src/app/admin-view/start/start.component.spec.ts
+++ src/app/admin-view/start/start.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StartComponent } from './start.component';
+import { StartAdminComponent } from './start.component';
 
 describe('StartComponent', () => {
-  let component: StartComponent;
-  let fixture: ComponentFixture<StartComponent>;
+  let component: StartAdminComponent;
+  let fixture: ComponentFixture<StartAdminComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [StartComponent]
+      declarations: [StartAdminComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(StartComponent);
+    fixture = TestBed.createComponent(StartAdminComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
