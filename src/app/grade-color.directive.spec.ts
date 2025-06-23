804333d89077ccf56a67b7082621baf966af6042
Jan Szumotalski <jszumotalski@gmail.com>
2025-06-23 12:05:08 +0200

    fix: grade color directive test

--- src/app/grade-color.directive.spec.ts
+++ src/app/grade-color.directive.spec.ts
@@ -1,8 +1,12 @@
+import { TestBed } from '@angular/core/testing';
 import { GradeColorDirective } from './grade-color.directive';
 
 describe('GradeColorDirective', () => {
   it('should create an instance', () => {
-    const directive = new GradeColorDirective();
+    let directive
+    TestBed.runInInjectionContext(() => {
+      directive = new GradeColorDirective();
+    })
     expect(directive).toBeTruthy();
   });
 });
