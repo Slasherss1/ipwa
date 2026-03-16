202856fe49235264d04b2cb873d71cd455ae3d49
Jan Szumotalski <jszumotalski@gmail.com>
2026-03-16 10:58:26 +0100

    added doc

--- src/app/admin-view/toolbar/toolbar.service.ts
+++ src/app/admin-view/toolbar/toolbar.service.ts
@@ -4,7 +4,30 @@ import { Injectable } from '@angular/core'
   providedIn: 'root',
 })
 export class ToolbarService {
+  /**
+   * In order to use this variable properly:
+   *
+   * In component's constructor set the ToolbarService's comp property to the component instance like following:
+   * ```ts
+   * ToolbarService#comp = this
+   * ```
+   *
+   * In component's onDestroy:
+   * ```ts
+   * ToolbarService#comp = undefined
+   * ```
+   */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public comp?: any
+
+  /**
+   * @param check - whether to show the option
+   * @param fn - set the param to the name of the function that should be called when user clicks the link
+   *
+   * In similar fashion to comp, insert in component's onDestroy:
+   * ```ts
+   * ToolbarService#menu = undefined
+   * ```
+   */
   public menu?: { title: string; check?: boolean; icon?: string; fn: string }[]
 }
